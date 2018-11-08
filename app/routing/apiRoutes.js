38bab38929acdcd158aba14eb4a3eb93447c37ac
Dennis The Menace <sdennisp25@hotmail.com>
2018-11-07 21:51:29 -0700

    corrected path and removed node modules

--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -4,22 +4,29 @@ var friendsList = require("../data/friends.js");
 // Export API routes
 module.exports = function(app) {
   app.get("/api/friends", function(req, res) {
-    // res.json(friendsList);
+    res.json(friendsList);
   });
 
   // Add new friend entry
   app.post("/api/friends", function(req, res) {
     var userInput = req.body;
-    var userResponse = userInput.scores;
+		var userResponse = userInput.scores;
+		
+		var userMatch = "";
+		var friendMatch = "";
 		
     for (var i = 0; i < friendsList.length; i++) {
-			// console.log(friendsList[i])
-			// res.json(friendsList[i]);
+			var scoreDif = 0;
+			for (var j = 0; j < userResponse.length; j++) {
+				scoreDif += Math.abs(friendsList[i].scores[i] - userResponse[i]);
+
+				// console.log(friendsList[i])
+				friendsList.push(userInput);
+				res.json(friendsList[i]);
+			}
 		}
 	});
 };
 	
-	// for (var j = 0; j < userResponse.length; j++) {
 		// diff += Math.abs(friendsList[i].scores[j] - userResponse[j]);
     // Add new user
-    // friendsList.push(userInput);
