0d11bdcab273006d9df76f2d108e281775074970
Dennis The Menace <sdennisp25@hotmail.com>
2018-11-07 18:20:07 -0700

    correct upstream

--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -4,22 +4,22 @@ var friendsList = require("../data/friends.js");
 // Export API routes
 module.exports = function(app) {
   app.get("/api/friends", function(req, res) {
-    res.json(friendsList);
+    // res.json(friendsList);
   });
 
   // Add new friend entry
   app.post("/api/friends", function(req, res) {
     var userInput = req.body;
     var userResponse = userInput.scores;
-
+		
     for (var i = 0; i < friendsList.length; i++) {
-      for (var j = 0; j < userResponse.length; j++) {
-        diff += Math.abs(friendsList[i].scores[j] - userResponse[j]);
-      }
-    }
-
-    // Add new user
-    friendsList.push(userInput);
-    res.json({userInput, userResponse});
-  });
+			// console.log(friendsList[i])
+			// res.json(friendsList[i]);
+		}
+	});
 };
+	
+	// for (var j = 0; j < userResponse.length; j++) {
+		// diff += Math.abs(friendsList[i].scores[j] - userResponse[j]);
+    // Add new user
+    // friendsList.push(userInput);
