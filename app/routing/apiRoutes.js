4e80cd863e8d782b0d01734d4b20d7655e793012
Dennis The Menace <sdennisp25@hotmail.com>
2018-11-07 17:49:30 -0700

    corrected the origin path to git

--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -1,39 +1,25 @@
 // Import the list of friend entries
-var friends = require('../data/friends.js');
+var friendsList = require("../data/friends.js");
 
 // Export API routes
 module.exports = function(app) {
-	app.get('/api/friends', function(req, res) {
-		res.json(friends);
-	});
+  app.get("/api/friends", function(req, res) {
+    res.json(friendsList);
+  });
 
-	// Add new friend entry
-	app.post('/api/friends', function(req, res) {
-		var userInput = req.body;
-		var userResponse = userInput.scores;
-		var matchName = '';
-		var matchImage = '';
-		var totalDifference = 10000; 
+  // Add new friend entry
+  app.post("/api/friends", function(req, res) {
+    var userInput = req.body;
+    var userResponse = userInput.scores;
 
-		// Examine all existing friends in the list
-		for (var i = 0; i < friends.length; i++) {
-			var diff = 0;
-			for (var j = 0; j < userResponse.length; j++) {
-				diff += Math.abs(friends[i].scores[j] - userResponse[j]);
-			}
+    for (var i = 0; i < friendsList.length; i++) {
+      for (var j = 0; j < userResponse.length; j++) {
+        diff += Math.abs(friendsList[i].scores[j] - userResponse[j]);
+      }
+    }
 
-			if (diff < totalDifference) {
-				totalDifference = diff;
-				matchName = friends[i].name;
-				matchImage = friends[i].photo;
-
-				console.log(matchName);
-				console.log(matchImage);
-			}
-		}
-
-		// Add new user
-		friends.push(userInput);
-		res.json({status: 'OK', matchName: matchName, matchImage: matchImage});
-	});
+    // Add new user
+    friendsList.push(userInput);
+    res.json({userInput, userResponse});
+  });
 };
