2b95a97591bd8c6a331d36b364c1626d586ef561
Dennis The Menace <sdennisp25@hotmail.com>
2018-11-08 23:50:51 -0700

    user matched and found friend

--- app/data/friends.js
+++ app/data/friends.js
@@ -1,132 +1,51 @@
 var friendsList = [
-	{
-		name: "Ahmed",
-		photo: "https://media.licdn.com/mpr/mpr/shrinknp_400_400/p/6/005/064/1bd/3435aa3.jpg",
-		scores: [
-			5,
-			1,
-			4,
-			4,
-			5,
-			1,
-			2,
-			5,
-			4,
-			1
-		]
-	},
-	{
-		name: "David",
-		photo: "https://media.licdn.com/mpr/mpr/shrinknp_400_400/p/6/005/064/1bd/3435aa3.jpg",
-		scores: [
-			5,
-			1,
-			4,
-			4,
-			5,
-			1,
-			2,
-			5,
-			4,
-			1
-		]
-	},
-	{
-		name: "Jessica",
-		photo: "https://media.licdn.com/mpr/mpr/shrinknp_400_400/p/6/005/064/1bd/3435aa3.jpg",
-		scores: [
-			5,
-			1,
-			4,
-			4,
-			5,
-			1,
-			2,
-			5,
-			4,
-			1
-		]
-	},
-	{
-		name: "Taylor",
-		photo: "https://media.licdn.com/mpr/mpr/shrinknp_400_400/p/6/005/064/1bd/3435aa3.jpg",
-		scores: [
-			5,
-			1,
-			4,
-			4,
-			5,
-			1,
-			2,
-			5,
-			4,
-			1
-		]
-	},
-	{
-		name: "Andrea",
-		photo: "https://media.licdn.com/mpr/mpr/shrinknp_400_400/p/6/005/064/1bd/3435aa3.jpg",
-		scores: [
-			5,
-			1,
-			4,
-			4,
-			5,
-			1,
-			2,
-			5,
-			4,
-			1
-		]
-	},
-	{
-		name: "Trista",
-		photo: "https://media.licdn.com/mpr/mpr/shrinknp_400_400/p/6/005/064/1bd/3435aa3.jpg",
-		scores: [
-			5,
-			1,
-			4,
-			4,
-			5,
-			1,
-			2,
-			5,
-			4,
-			1
-		]
-	},
-	{
-		name: "Mike",
-		photo: "https://media.licdn.com/mpr/mpr/shrinknp_400_400/p/6/005/064/1bd/3435aa3.jpg",
-		scores: [
-			5,
-			1,
-			4,
-			4,
-			5,
-			1,
-			2,
-			5,
-			4,
-			1
-		]
-	},
-	{
-		name: "Jonus",
-		photo: "https://media.licdn.com/mpr/mpr/shrinknp_400_400/p/6/005/064/1bd/3435aa3.jpg",
-		scores: [
-			5,
-			1,
-			4,
-			4,
-			5,
-			1,
-			2,
-			5,
-			4,
-			1
-		]
-	}
+  {
+    name: "Ahmed",
+    photo:
+      "https://i.redd.it/so5cg125lh001.jpg",
+    scores: [5, 1, 4, 4, 5, 1, 2, 5, 4, 1]
+  },
+  {
+    name: "David",
+    photo:
+      "https://i.redd.it/so5cg125lh001.jpg",
+    scores: [3, 4, 6, 6, 3, 3, 6, 7, 8, 7]
+  },
+  {
+    name: "Jessica",
+    photo:
+      "https://i.redd.it/so5cg125lh001.jpg",
+    scores: [1, 2, 3, 4, 5, 6, 7, 8, 9, 1]
+  },
+  {
+    name: "Taylor",
+    photo:
+      "https://i.redd.it/so5cg125lh001.jpg",
+    scores: [1, 1, 2, 2, 3, 3, 4, 4, 5, 5]
+  },
+  {
+    name: "Andrea",
+    photo:
+      "https://i.redd.it/so5cg125lh001.jpg",
+    scores: [1, 2, 3, 4, 1, 2, 3, 4, 1, 2]
+  },
+  {
+    name: "Trista",
+    photo:
+      "https://i.redd.it/so5cg125lh001.jpg",
+    scores: [9, 5, 6, 7, 3, 2, 5, 6, 7, 4]
+  },
+  {
+    name: "Mike",
+    photo:
+      "https://i.redd.it/so5cg125lh001.jpg",
+    scores: [4, 5, 6, 7, 8, 1, 2, 3, 5, 5]
+  },
+  {
+    name: "Jonus",
+    photo: "https://i.redd.it/so5cg125lh001.jpg",
+    scores: [7, 4, 3, 6, 7, 8, 9, 1, 4, 4]
+  }
 ];
 
 module.exports = friendsList;
--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -1,32 +1,40 @@
-// Import the list of friend entries
-var friendsList = require("../data/friends.js");
+// // Import the list of friend entries
+var friendsList = require("../data/friends");
+var path = require("path");
 
-// Export API routes
+// // Export API routes
 module.exports = function(app) {
-  app.get("/api/friends", function(req, res) {
+  app.get("api/friends", function(req, res) {
     res.json(friendsList);
   });
 
-  // Add new friend entry
+	//finds your friend
   app.post("/api/friends", function(req, res) {
     var userInput = req.body;
-		var userResponse = userInput.scores;
-		
-		var userMatch = "";
-		var friendMatch = "";
-		
-    for (var i = 0; i < friendsList.length; i++) {
-			var scoreDif = 0;
-			for (var j = 0; j < userResponse.length; j++) {
-				scoreDif += Math.abs(friendsList[i].scores[i] - userResponse[i]);
+    var yourMatch = {
+      name: "",
+      photo: "",
+      diff: 100
+    };
 
-				// console.log(friendsList[i])
-				friendsList.push(userInput);
-				res.json(friendsList[i]);
-			}
-		}
-	});
+    // runs loop through friends list
+    for (var i = 0; i < friendsList.length; i++) {
+      scoreTotal = 0;
+			// runs loop through the friendslist scores
+      for (var j = 0; j < friendsList[i].scores.length; j++) {
+				// compare the users score to the friends score. Takes the absolute value from the difference.
+        scoreTotal += Math.abs(friendsList[i].scores[j] - userInput.scores[j]);
+
+				// condition to find our match and set the values
+        if (scoreTotal <= yourMatch.diff) {
+          (yourMatch.name = friendsList[i].name),
+          (yourMatch.photo = friendsList[i].photo),
+          (yourMatch.diff = scoreTotal);
+        }
+      }
+    }
+    friendsList.push(userInput);
+    res.json(yourMatch);
+    console.log(yourMatch);
+  });
 };
-	
-		// diff += Math.abs(friendsList[i].scores[j] - userResponse[j]);
-    // Add new user
